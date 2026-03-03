9ca49c98e9a5d6c37a60fb85c84f21eae688b7b7
fifth_light <liu-yu-jie@foxmail.com>
2026-03-03 22:36:13 +0800

    [TouchController] 修复文本输入支持

--- touchcontroller/common/ui/theme/TouchControllerTheme.kt
+++ touchcontroller/common/ui/theme/TouchControllerTheme.kt
@@ -3,6 +3,7 @@ package top.fifthlight.touchcontroller.common.ui.theme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
+import top.fifthlight.combine.node.LocalInputHandler
 import top.fifthlight.combine.paint.BackgroundTexture
 import top.fifthlight.combine.paint.Drawable
 import top.fifthlight.combine.theme.LocalTheme
@@ -12,6 +13,7 @@ import top.fifthlight.combine.theme.blackstone.BlackstoneTheme
 import top.fifthlight.combine.ui.style.ColorTheme
 import top.fifthlight.combine.ui.style.DrawableSet
 import top.fifthlight.touchcontroller.assets.Textures
+import top.fifthlight.touchcontroller.common.input.InputManager
 
 val LocalTouchControllerTheme = staticCompositionLocalOf { TouchControllerTheme() }
 
@@ -89,6 +91,7 @@ data class TouchControllerTheme(
 inline operator fun TouchControllerTheme.invoke(crossinline block: @Composable TouchControllerTheme.() -> Unit) {
     CompositionLocalProvider(
         LocalTouchControllerTheme provides TouchControllerTheme(),
+        LocalInputHandler provides InputManager,
         LocalTheme provides base,
     ) {
         block()
